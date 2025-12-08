8c8d760abc6459d33fed484af0354b9a1eb52784
mattchristenson <matt@rcc.team>
2025-12-08 14:00:08 -0600

    finished products not tested yet

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/models/Product.java
@@ -0,0 +1,62 @@
+package com.pluralsight.NorthwindTradersAPI.models;
+
+public class Product {
+    private int productId;
+    private String name;
+    //private String category;
+    private int categoryId;
+    private double price;
+
+    public Product(){
+        //do nothing;
+    }
+
+    public Product(int productId, String name, int categoryId, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.categoryId = categoryId;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", categoryId='" + categoryId + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
